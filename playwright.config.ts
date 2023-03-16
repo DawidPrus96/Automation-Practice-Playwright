dda0e5bda2ac1f10cd62ce85d206500ad49c2943
DawidPrus96 <dawid.prus96@gmail.com>
2023-03-16 20:38:55 +0100

    50% of workers for github

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   retries: 3,
   //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 3,
+  workers: '50%',
   //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
