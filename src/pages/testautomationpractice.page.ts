6cad4e515ecdb69f6e3275cb74ac4eaae2496ed3
DawidPrus96 <dawid.prus96@gmail.com>
2023-03-23 22:54:21 +0100

    Calendar

--- src/pages/testautomationpractice.page.ts
+++ src/pages/testautomationpractice.page.ts
@@ -1,34 +1,85 @@
 // playwright-dev-page.ts
-import { expect, Page } from '@playwright/test';
+import { expect, Page, Locator } from '@playwright/test';
 export class AutomationTools {
     readonly page: Page;
     readonly websocket: WebSocket;
+    readonly locWiki: Locator;
+    readonly locAlert: Locator;
+    readonly locDatePickerInput: Locator;
+    readonly locDatePickerCalendar: Locator;
     constructor(page: Page) {
         this.page = page;
+        this.locWiki = page.locator('#Wikipedia1')
+        this.locAlert = page.locator('#HTML9')
+        this.locDatePickerInput = page.locator('#HTML5')
+        this.locDatePickerCalendar = page.locator('#ui-datepicker-div')
+
+        // this.getStartedLink = page.locator('a', { hasText: 'Get started' });
+        // this.gettingStartedHeader = page.locator('h1', { hasText: 'Installation' });
+        // this.pomLink = page.locator('li', { hasText: 'Guides' }).locator('a', { hasText: 'Page Object Model' });
+        // this.tocList = page.locator('article div.markdown ul > li > a');
     }
     async searchWikipediaPhrase(phrase: string) {
-        await this.page.locator('#Wikipedia1_wikipedia-search-input').fill(phrase)
-        await this.page.getByRole('button', { name: 'submit' }).click()
-        await expect(this.page.locator('#Wikipedia1_wikipedia-search-results-header')).toBeVisible()
-        return await this.page.locator('#Wikipedia1_wikipedia-search-results').getByRole('link').count()
+        await this.locWiki.getByRole('textbox').fill(phrase)
+        await this.locWiki.getByRole('button').click()
+        if (!phrase)
+            await expect(this.locWiki.getByText('Please enter text to search.')).toBeVisible()
+        else
+            await expect(this.locWiki.getByText('Search results')).toBeVisible()
+        return await this.locWiki.locator('#Wikipedia1_wikipedia-search-results').getByRole('link').count()
     }
     async GoToFirstWikipediaArticle() {
         const wikipagePromise = this.page.waitForEvent('popup');
-        const article = this.page
+        const article = this.locWiki
             .locator('#Wikipedia1_wikipedia-search-results')
             .getByRole('link')
             .first()
-        let phrase = await article.innerText()
+        const phrase = await article.innerText()
         await article.click()
         const wikipage = await wikipagePromise;
         await wikipage.waitForLoadState();
         await expect(wikipage.locator('#firstHeading')).toHaveText(phrase)
-    } async GoToMoreWikipediaResults(phrase: string) {
+    }
+    async GoToMoreWikipediaResults(phrase: string) {
         const wikipagePromise = this.page.waitForEvent('popup');
-        const more = this.page.getByRole('link', { name: 'more' })
+        const more = this.locWiki.getByRole('link', { name: 'more' })
         await more.click();
         const wikipage = await wikipagePromise;
         await wikipage.waitForLoadState();
         await expect(wikipage.locator(`input[value="${phrase}"][role="combobox"]`)).toBeVisible()
+    }
+    async dialog(accept: boolean) {
+        this.page.once('dialog', dialog => {
+            if (accept)
+                dialog.accept()
+            else
+                dialog.dismiss()
+        });
+        await this.locAlert.getByRole('button', { name: 'Click Me' }).click()
+        if (accept)
+            await this.locAlert.getByText('You pressed OK!').click()
+        else
+            await this.locAlert.getByText('You pressed Cancel!').click()
+    }
+    async pickDate(date: Date) {
+        let today = new Date()
+        let clicks = (today.getFullYear() - date.getFullYear()) * 12 + (today.getMonth() - date.getMonth())
+        await this.locDatePickerInput.getByRole('textbox').click()
+        if (clicks > 0) {
+            for (let i = 0; i < clicks; i++) {
+                await this.locDatePickerCalendar.locator('a').filter({ hasText: 'Prev' }).click()
+            }
+        }
+        else {
+            for (let i = 0; i < Math.abs(clicks); i++) {
+                await this.locDatePickerCalendar.locator('a').filter({ hasText: 'Next' }).click()
+            }
+        }
+        await this.locDatePickerCalendar.getByRole('link', { name: `${date.getUTCDate()}`, exact: true }).click()
+        await this.locDatePickerInput.getByRole('textbox').click()
+        await expect(this.locDatePickerCalendar
+            .locator(`td[data-month="${date.getUTCMonth()}"][data-year="${date.getFullYear()}"] > .ui-state-active`)
+            .getByText(`${date.getUTCDate()}`))
+            .toBeVisible()
     }
 }
--- src/tests/testautomationpractice.spec.ts
+++ src/tests/testautomationpractice.spec.ts
@@ -8,6 +8,12 @@ test.beforeEach(async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Automation Testing Practice', level: 1 })).toBeVisible()
 })
 test.describe('Wikipedia', () => {
+    test('Empty phrase', async ({ page }) => {
+        const playwrightDev = new AutomationTools(page);
+        let phrase = ''
+        let count = await playwrightDev.searchWikipediaPhrase(phrase)
+        expect(count).toEqual(0)
+    });
     test('1 specific result', async ({ page }) => {
         const playwrightDev = new AutomationTools(page);
         let phrase = 'Glacial lake outburst flood'
@@ -28,5 +34,22 @@ test.describe('Wikipedia', () => {
         let count = await playwrightDev.searchWikipediaPhrase(phrase)
         expect(count).toEqual(0)
         await expect(page.locator('#Wikipedia1_wikipedia-search-results')).toHaveText('No results found.')
+    });
+})
+test.describe('Alert', () => {
+    test('Accept', async ({ page }) => {
+        const playwrightDev = new AutomationTools(page);
+        await playwrightDev.dialog(true)
+    });
+    test('Cancel', async ({ page }) => {
+        const playwrightDev = new AutomationTools(page);
+        await playwrightDev.dialog(false)
+    });
+})
+test.describe('Date Picker', () => {
+    test.only('Select date from calendar', async ({ page }) => {
+        const date = new Date("2019-09-01");
+        const playwrightDev = new AutomationTools(page);
+        await playwrightDev.pickDate(date)
     });
 })
